19046bc608161bb2c0c2758483bfcd4e44a9a714
goofy.l <goofy.l@hpcnt.com>
2026-05-06 20:36:27 +0900

    finish game2

--- game2_skeleton.py
+++ game2_skeleton.py
@@ -1,9 +1,7 @@
 """
-게임 2: 피하기 게임 - 스켈레톤 (학생용)
+게임 2: 피하기 게임 - 완성본
 목표: 장애물을 피하면서 최대한 오래 생존하기
 점수 = 생존 시간(초)
-
-아래의 TODO 부분을 완성해서 게임을 만들어보세요!
 """
 
 import pgzrun
@@ -24,34 +22,31 @@ player.x = WIDTH // 2
 player.y = HEIGHT - 50
 
 # 장애물 리스트
-# TODO 1: obstacles라는 빈 리스트를 만드세요
-obstacles = ___________________
+obstacles = []
 
 # 게임 상태
 frame_count = 0
 game_over = False
 obstacle_speed = OBSTACLE_SPEED
 
-# 초기 장애물 생성
-# TODO 2: for 반복문을 사용해서 obstacles 리스트에 장애물 3개를 추가하세요
-# (각 장애물은 Actor('obstacle')이고, x는 랜덤, y는 음수로 위쪽에 배치)
+# 초기 장애물 3개 생성
 for i in range(3):
     obs = Actor('obstacle')
-    obs.x = ___________________
-    obs.y = ___________________
+    obs.x = random.randint(32, WIDTH - 32)
+    obs.y = -50 - (i * 100)  # 위쪽에서 순서대로 배치
     obstacles.append(obs)
 
 def draw():
     screen.clear()
     screen.fill((25, 25, 60))  # 어두운 파란색 배경
-    
+
     if not game_over:
-        # TODO 3: for 반복문으로 모든 장애물을 그리세요 (obstacle.draw())
-        for obstacle in ___________________:
-            ___________________
-        
+        # 모든 장애물 그리기
+        for obstacle in obstacles:
+            obstacle.draw()
+
         player.draw()
-        
+
         # 생존 시간 표시
         survival_time = frame_count // 60  # 프레임을 초로 변환
         screen.draw.text(f"Survival: {survival_time}s", (20, 20), fontsize=30, color=(255, 255, 255))
@@ -65,74 +60,71 @@ def draw():
 
 def update():
     global frame_count, game_over, obstacle_speed
-    
+
     if game_over:
         if keyboard.r:
             reset_game()
         if keyboard.q:
             exit()
         return
-    
+
     frame_count += 1
-    
+
     # 플레이어 이동 (좌)
     if keyboard.left:
         player.x -= SPEED
-    
+
     # 플레이어 이동 (우)
     if keyboard.right:
         player.x += SPEED
-    
+
     # 플레이어 경계 체크
     if player.x < 32:
         player.x = 32
     if player.x > WIDTH - 32:
         player.x = WIDTH - 32
-    
-    # TODO 4: for 반복문으로 모든 장애물의 y 좌표를 obstacle_speed만큼 증가시키세요
-    for obstacle in ___________________:
-        obstacle.y += ___________________
-    
-    # TODO 5: 화면 밖으로 나간 장애물을 obstacles 리스트에서 제거하세요
-    # (먼저 리스트 복사본을 순회해야 안전함: for obstacle in obstacles[:])
+
+    # 모든 장애물 업데이트
+    for obstacle in obstacles:
+        obstacle.y += obstacle_speed
+
+    # 화면 밖 장애물 제거
     obstacles_to_remove = []
-    for obstacle in obstacles[:]:
+    for obstacle in obstacles:
         if obstacle.y > HEIGHT:
-            ___________________
-    
+            obstacles_to_remove.append(obstacle)
+
     for obstacle in obstacles_to_remove:
-        ___________________
-    
-    # TODO 6: player와 모든 장애물의 충돌을 확인하세요
-    # (for 반복문 사용, colliderect 사용, 충돌하면 game_over = True)
-    for obstacle in ___________________:
-        if ___________________:
+        obstacles.remove(obstacle)
+
+    # 충돌 감지
+    for obstacle in obstacles:
+        if player.colliderect(obstacle):
             game_over = True
             return
-    
-    # TODO 7: frame_count가 180의 배수일 때마다 새 장애물을 추가하세요
-    # (3초마다 = 60fps × 3초 = 180프레임)
-    if ___________________:
+
+    # 일정 시간마다 새 장애물 추가 (3초마다 = 180프레임)
+    if frame_count % 180 == 0:
         new_obstacle = Actor('obstacle')
         new_obstacle.x = random.randint(32, WIDTH - 32)
         new_obstacle.y = 0
         obstacles.append(new_obstacle)
-    
-    # TODO 8: frame_count가 600의 배수일 때마다 obstacle_speed를 1 증가시키세요
-    # (10초마다 = 60fps × 10초 = 600프레임)
-    if ___________________:
-        obstacle_speed += ___________________
+
+    # 난이도 증가 (10초마다 속도 +1)
+    if frame_count % 600 == 0:  # 600프레임 = 10초
+        obstacle_speed += 1
 
 def reset_game():
     global frame_count, game_over, obstacle_speed, obstacles
     frame_count = 0
     game_over = False
     obstacle_speed = OBSTACLE_SPEED
-    
+
     player.x = WIDTH // 2
     player.y = HEIGHT - 50
-    
+
     obstacles = []
+    # 초기 장애물 3개 생성
     for i in range(3):
         obs = Actor('obstacle')
         obs.x = random.randint(32, WIDTH - 32)
